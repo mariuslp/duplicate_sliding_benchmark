b26b36323c3fbc249111773a67eccd12eccfe752
MLP <marius.lombard-platet@almerys.com>
2020-03-12 15:41:08 +0100

    fixes

--- src/include/queue_filters/queue_qqhtd.h
+++ src/include/queue_filters/queue_qqhtd.h
@@ -8,16 +8,11 @@ private:
     void create_fresh_filter(); 
 
 protected:
-    size_t number_filters;
-    size_t sliding_window;
     size_t n_buckets;
     size_t fingerprint_size;
-
 
 public:
     QueueQQHTDFilter(size_t memory_size, size_t n_n_buckets, size_t n_fingerprint_size, size_t n_number_filters, size_t n_sliding_window);
     QQHTDCompactFilter Init() const;
-    void debug() const;
-    void Reset();
     std::string name() const { return "Queueing QQHTD"; };
 };
--- src/queue_filters/queue_qqhtd.cpp
+++ src/queue_filters/queue_qqhtd.cpp
@@ -2,6 +2,7 @@
 
 void QueueQQHTDFilter::create_fresh_filter() {
     queue.push_front(std::make_unique<QQHTDCompactFilter>(memory_per_filter, n_buckets, fingerprint_size));
+    queue[0]->Reset();
 }
 
 QueueQQHTDFilter::QueueQQHTDFilter(size_t memory_size, 
@@ -9,11 +10,11 @@ QueueQQHTDFilter::QueueQQHTDFilter(size_t memory_size,
             size_t n_fingerprint_size,
             size_t n_number_filters,
             size_t n_sliding_window) : 
-        number_filters(n_number_filters), 
-        sliding_window(n_sliding_window),
         n_buckets(n_n_buckets), 
         fingerprint_size(n_fingerprint_size) {
 
+    number_filters = n_number_filters;
+    sliding_window = n_sliding_window;
     Log("Total memory: ", memory_size, ", n_filters: ", n_number_filters);
     Log("Sliding window ", sliding_window);
     memory_per_filter = memory_size / n_number_filters;
@@ -30,18 +31,4 @@ QueueQQHTDFilter::QueueQQHTDFilter(size_t memory_size,
 
 QQHTDCompactFilter QueueQQHTDFilter::Init() const {
     return QQHTDCompactFilter(memory_per_filter, n_buckets, fingerprint_size);
-}
-
-
-void QueueQQHTDFilter::debug() const {
-    for(size_t i = 0; i < number_filters; ++i) {
-        queue[i]->debug();
-    }
-}
-
-void QueueQQHTDFilter::Reset() {
-    for(size_t i = 0; i < number_filters; ++i) {
-        queue[i]->Reset();
-        counter = 0;
-    }
 }
